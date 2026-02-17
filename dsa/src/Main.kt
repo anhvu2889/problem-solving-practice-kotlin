41328fe9a826b834812f53603d7a8a22ff2f7f2d
Vu Nguyen <crisnguyendev@gmail.com>
2026-02-17 00:11:48 -0800

    Complete Two Pointers problems 977. Squares of a Sorted Array 2486. Append Characters to String to Make Subsequence

--- dsa/src/Main.kt
+++ dsa/src/Main.kt
@@ -1,13 +1,14 @@
-import blind75.dp.NthTribonacciNumber
-import top150.array.BestTimeBuySellStockII
-import top150.array.JumpGameII
+import topic.twopointer.AppendCharactersStringMakeSubsequence
+import topic.twopointer.SquaresOfSortedArray
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var nums1 = intArrayOf(2,3,1,1,4)
-    var nums2 = intArrayOf(1, 2, 3, 4, 5)
+    var nums1 = intArrayOf(-4, -1, 0, 3, 10)
+    var nums2 = intArrayOf(-7, -3, 2, 3, 11)
     var nums3 = intArrayOf(7, 6, 4, 3, 1)
-    val result = NthTribonacciNumber().tribonacci(2)
-    println(result)
+    var s = "z"
+    var t = "abcde"
+    val result = SquaresOfSortedArray().sortedSquares(nums2)
+    println(result.contentToString())
 }
