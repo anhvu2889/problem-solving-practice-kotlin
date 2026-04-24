5d411129abf9ec2d27b6f6abd21d2e6bdaf277f0
Vu Nguyen <crisnguyendev@gmail.com>
2026-04-23 23:51:46 -0700

    Complete Blind 75 -  DP 1D problems
    790. Domino and Tromino Tiling
    198. House Robber
    1137. N-th Tribonacci Number
    746. Min Cost Climbing Stairs

--- dsa/src/blind75/dp/HouseRobber.kt
+++ dsa/src/blind75/dp/HouseRobber.kt
@@ -1,7 +1,21 @@
 package blind75.dp
 
+/**
+ * 198. House Robber
+ * Time: O(n)
+ * Space: O(1)
+ */
 class HouseRobber {
     fun rob(nums: IntArray): Int {
-        return -1
+        if (nums.size == 1)
+            return nums[0]
+        var prev2 = nums[0]
+        var prev1 = maxOf(nums[0], nums[1])
+        for (i in 2 until nums.size) {
+            val cur = maxOf(prev1, nums[i] + prev2)
+            prev2 = prev1
+            prev1 = cur
+        }
+        return prev1
     }
 }
--- dsa/src/blind75/dp/MinCostClimbingStairs.kt
+++ dsa/src/blind75/dp/MinCostClimbingStairs.kt
@@ -5,18 +5,19 @@ import kotlin.math.min
 
 /**
  * 746. Min Cost Climbing Stairs
- * Time: O(n)\
- * Space: O(n)
+ * Time: O(n)
+ * Space: O(1)
  */
 class MinCostClimbingStairs {
     fun minCostClimbingStairs(cost: IntArray): Int {
         val n = cost.size
-        var dp = IntArray(n)
-        dp[0] = cost[0]
-        dp[1] = cost[1]
+        var prev2 = cost[0]
+        var prev1 = cost[1]
         for(i in 2 until n) {
-            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+            val cur = min(prev1 + cost[i], prev2 + cost[i])
+            prev2 = prev1
+            prev1 = cur
         }
-        return min(dp[n - 1], dp[n - 2])
+        return min(prev1, prev2)
     }
 }
